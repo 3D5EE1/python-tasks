7f6669af910119bd639fad56746599c2cf153cc0
Evgeny-Gurin <rypylook@outlook.com>
2018-11-24 18:29:01 +0500

    print list

---
+++ print list.py
@@ -0,0 +1,6 @@
+# question: list = [1, 2, 3, 4, 'q', '{', 'r', 'T', ']', 'nhg', '1r#', ]
+# result: 1, 2, 3, 4, q, {, r, T, ], nhg, 1r#.
+
+list_ = [1, 2, 3, 4, 'q', '{', 'r', 'T', ']', 'nhg', '1r#', ]
+
+print(*list_[:-1], str(list_[-1]) + '.',sep=', ')
